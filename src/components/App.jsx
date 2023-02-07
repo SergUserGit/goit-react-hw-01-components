3520ef7bebe00304397e34e0789bb1ff70fef50d
Sergii Podriezienko <podreenko1986@gmail.com>
2023-02-07 11:39:45 +0200

    part 4 start
    part 4 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendsList from './Friends/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './Friends/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -28,6 +30,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
