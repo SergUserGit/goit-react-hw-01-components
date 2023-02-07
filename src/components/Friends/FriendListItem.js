a2f29ebc3e50c97cb32d8f191a99a062dcf103dd
Sergii Podriezienko <podreenko1986@gmail.com>
2023-02-07 10:55:57 +0200

    part 3 css
    part 3 css

--- src/components/Friends/FriendListItem.js
+++ src/components/Friends/FriendListItem.js
@@ -3,7 +3,11 @@ import css from './Friends.module.css';
 export default function FriendListItem({ isOnline, avatar, name }) {
   return (
     <li className={css.item}>
-      <span className={isOnline ? css.statusOnline : css.status}></span>
+      <span
+        className={
+          css.status + ' ' + css[isOnline ? 'statusOnline' : 'statusOffline']
+        }
+      ></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
