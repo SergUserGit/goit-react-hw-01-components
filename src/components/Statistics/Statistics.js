135e1f7559f2ff4ee3565679233c04f52d91288f
Sergii Podriezienko <podreenko1986@gmail.com>
2023-02-08 15:35:03 +0200

    Statistics
    Statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-export default function Statistics({ title, stats }) {
+const Statistics = function ({ title, stats }) {
   return (
     <section className={css.statistics}>
       {typeof title !== 'undefined' && <h2 className={css.title}>{title}</h2>}
@@ -14,4 +15,17 @@ export default function Statistics({ title, stats }) {
       </ul>
     </section>
   );
-}
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+};
+
+export default Statistics;
